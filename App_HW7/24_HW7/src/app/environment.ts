52d14ce6239293b8d95fc767ec5a8c2353580eb7
Oscar Tzeng <104820003>
2018-06-03 10:15:47 +0800

    ignore environment.ts

--- App_HW7/24_HW7/src/app/environment.ts
+++ App_HW7/24_HW7/src/app/environment.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "<Your Firebase apiKey>",
-    authDomain: "<Your Firebase authDomain>",
-    databaseURL: "<Your Firebase databaseURL>",
-    projectId: "<Your Project ID>",
-    storageBucket: "<Your Firebase storageBucket>",
-    messagingSenderId: "<YOur Firebase messagingSenderId>"
+    apiKey: "AIzaSyC0WHsCyjBxLPH7-HzCQOVs7KOJUQI8XCo",
+    authDomain: "apps2018-104820003.firebaseapp.com",
+    databaseURL: "https://apps2018-104820003.firebaseio.com",
+    projectId: "apps2018-104820003",
+    storageBucket: "apps2018-104820003.appspot.com",
+    messagingSenderId: "44272689722"
 }
